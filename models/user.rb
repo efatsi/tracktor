979762220492cc15729da1f3181f9606bf7c5e41
Eli Fatsi <eli.fatsi@viget.com>
2013-11-08 21:16:03 -0700

    Authenticating new Users against Harvest API

--- models/user.rb
+++ models/user.rb
@@ -11,7 +11,7 @@ class User
   before :create, :generate_token
 
   validates_presence_of :domain, :email, :password
-  # validate :authenticated_with_harvest
+  validates_with_method :valid_harvest_account
 
   def self.first_or_create(params)
     if existing = first(:email => params[:email])
@@ -40,6 +40,19 @@ class User
 
   def generate_token
     self.token = SecureRandom.urlsafe_base64(5)
+  end
+
+  def valid_harvest_account
+    begin
+      client.account.who_am_i
+      return true
+    rescue Harvest::AuthenticationFailed
+      [false, "Invalid email password combination"]
+    end
+  end
+
+  def client
+    @client ||= Harvest.client(domain, email, password)
   end
 
 end
